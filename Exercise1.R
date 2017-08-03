6ac4ed07aa83c483f897fc7eb03bdc61decdb17e
yelaura <laura.ye.lin@gmail.com>
2017-08-02 20:22:14 -0700

    First commit

---
+++ Exercise1.R
@@ -0,0 +1,8 @@
+data(mtcars)                           #Load mtcars data set
+plot(mtcars$hp, mtcars$mpg,            #Plot hp as x, and mpg as y
+     xlab="Horsepower",                #Define X-axis label
+     ylab="Gas Mileage (mpg)",         #Define Y-axis label
+     col = "blue",                     #Define color of data points
+     main = "Gas Mileage vs HP",       #Define main title of plot
+     sub = "Gas mileage is in MPG",    #Define sub title of plot
+     pch=6)                            #We want the symbol to be triangles (arbitrary)
