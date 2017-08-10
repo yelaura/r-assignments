57134ca921a9c782de05eb7f93356de578125490
yelaura <laura.ye.lin@gmail.com>
2017-08-09 20:34:35 -0700

    Added Exercise2.R

--- Exercise1.R
+++ Exercise1.R
@@ -5,4 +5,8 @@ plot(mtcars$hp, mtcars$mpg,            #Plot hp as x, and mpg as y
      col = "blue",                     #Define color of data points
      main = "Gas Mileage vs HP",       #Define main title of plot
      sub = "Gas mileage is in MPG",    #Define sub title of plot
-     pch=6)                            #We want the symbol to be triangles (arbitrary)
+     pch=6)                            #We want the symbol to be triangles (pch=6)
+
+# Observations:
+# Plot shows a downward exponential trend. 
+# As the horsepower increases the gas mileage decreases.
---
+++ Exercise2.R
@@ -0,0 +1,36 @@
+# 1. Downloads the data from this link and saves it as a data.frame
+
+library("RCurl")
+rawEQ <- getURL("https://raw.githubusercontent.com/fivethirtyeight/data/master/forecast-methodology/historical-senate-predictions.csv")
+dfEQ <- read.csv(text = rawEQ)
+
+# 2. Replaces the redundant 'Lose' level for the result column with 'Loss'
+
+lvls <- factor(dfEQ$result)
+levels(lvls) <- c("Loss", "Loss", "Win")
+
+dfEQ$result <- lvls
+
+# 3. Subsets the data to only the year 2012
+
+dfEQ_2012 <- subset(dfEQ, year==2012)
+ 
+# 4. Creates a box plot of forecast_prob per result
+# 5. Saves the box plot as a .png file
+
+win <- subset(dfEQ_2012, result=="Win")
+lose <- subset(dfEQ_2012, result == "Loss")
+
+png("boxplot.png")
+boxplot(win$forecast_prob, lose$forecast_prob, names=c("Win", "Loss"), ylab="Forecast Probability")
+dev.off()
+
+# 6. Saves the modified data.frame to a .rData file
+
+save(dfEQ_2012, file="dfEQ.rData")
+
+# Checking that the png can be loaded
+
+library(imager)
+im <- load.image("boxplot.png")
+show(im)
